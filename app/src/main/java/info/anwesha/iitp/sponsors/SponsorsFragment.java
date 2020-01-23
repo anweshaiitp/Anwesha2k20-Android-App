53de7facda4809a0eef8b91620785c38e74e5e9a
somenath sarkar <somenath1435@gmail.com>
2020-01-24 00:23:29 +0530

    Fixed build errors

--- app/src/main/java/info/anwesha/iitp/sponsors/SponsorsFragment.java
+++ app/src/main/java/info/anwesha/iitp/sponsors/SponsorsFragment.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
