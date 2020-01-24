c33d7754a2a13486318ec6c8801ac5c3a8fed410
somenath sarkar <somenath1435@gmail.com>
2020-01-25 00:13:36 +0530

    Added live feed Recycler view

--- app/src/main/java/info/anwesha/iitp/livefeed/LiveFeedResponse.java
+++ app/src/main/java/info/anwesha/iitp/livefeed/LiveFeedResponse.java
@@ -17,25 +17,3 @@ public class LiveFeedResponse {
     }
 }
 
-class NewsList {
-    @SerializedName("news")
-    private String news;
-    @SerializedName("img")
-    private String img;
-
-    public String getNews() {
-        return news;
-    }
-
-    public void setNews(String news) {
-        this.news = news;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-}
