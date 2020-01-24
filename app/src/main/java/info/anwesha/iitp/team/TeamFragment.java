c90213da4b887ad03e546fb2ff872083a9663110
somenath sarkar <43093724+somenath1435@users.noreply.github.com>
2020-01-25 03:51:35 +0530

    Changed order in team page and fix margin issue (#61)

--- app/src/main/java/info/anwesha/iitp/team/TeamFragment.java
+++ app/src/main/java/info/anwesha/iitp/team/TeamFragment.java
@@ -36,18 +36,18 @@ public class TeamFragment extends Fragment {
 
         List<info.anwesha.iitp.team.TeamItem> allMembers = new ArrayList<>();
 
-        allMembers.add(new info.anwesha.iitp.team.TeamItem("Mayank Wadhwani", "Fest Convener", "8181068781", "https://www.facebook.com/mayank.wadhwani", R.drawable.mayank_wadhwani));
+//        allMembers.add(new info.anwesha.iitp.team.TeamItem("Mayank Wadhwani", "Fest Convener", "8181068781", "https://www.facebook.com/mayank.wadhwani", R.drawable.mayank_wadhwani));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Pranil Kesaralikar", "Overall Fest Coordinator", "6201169731", "https://www.facebook.com/pranil.kesaralikar", R.drawable.pranil));
-        allMembers.add(new info.anwesha.iitp.team.TeamItem("Shivansh Shukla", "Pro Show Coordinator ", "8707499083", "https://facebook.com/shivansh.shukla.50", R.drawable.shivansh));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Hitvardhan ", "Coordinator - Marketing and Sponsorship", "7761891077", "https://m.facebook.com/vardhan.hit", R.drawable.hitvardhan));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Nandan Shah ", "Coordinator - Marketing and Sponsorship", "0", "https://m.facebook.com/anwesha.iitpatna", R.drawable.nandan));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Mridul Srivastava ", "Coordinator - Marketing and Sponsorship", "0", "https://www.facebook.com/mridul.srivastava.3192", R.drawable.mridul));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Himanshu Gupta", "Coordinator - Media and Public Relations", "9521205729", "https://www.facebook.com/himgupta134", R.drawable.himanshu_gupta));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Kartik Singh", "Coordinator - Media and Public Relations", "8077516207", "https://www.facebook.com/profile.php?id=100005570138494", R.drawable.karthik_singh));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Bhavani Susmitha", "Coordinator - Media and Public Relations", "9123519137", "https://m.facebook.com/bhavani.susmitha", R.drawable.bhavani_susmitha));
-        allMembers.add(new info.anwesha.iitp.team.TeamItem("Vatsal Singhal", "Coordinator - Development Team", "8585992062", "https://m.facebook.com/vatsalsinghal1", R.drawable.vatsal_singhal));
+        allMembers.add(new info.anwesha.iitp.team.TeamItem("Shivansh Shukla", "Pro Show Coordinator ", "8707499083", "https://facebook.com/shivansh.shukla.50", R.drawable.shivansh));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Kshitij Jain", "Coordinator - Development Team", "9131604185", "https://www.facebook.com/kshitij.jain.39904", R.drawable.kshitij_jain));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Adarsh Chaudhary ", "Coordinator - Development Team", "8827275145", "https://www.facebook.com/adarsh217", R.drawable.adarsh_chaudhary));
+        allMembers.add(new info.anwesha.iitp.team.TeamItem("Vatsal Singhal", "Coordinator - Development Team", "8585992062", "https://m.facebook.com/vatsalsinghal1", R.drawable.vatsal_singhal));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Rajeshwar Ojha", "Coordinator - Creatives And Design", "07000125484", "https://www.facebook.com/rajeshwar.ojha.50", R.drawable.rajeshwar));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Utkarsh", "Coordinator - Creatives And Design", "08987044093", "https://www.facebook.com/utkarsh.virtuous", R.drawable.utkarsh));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Nikhil George", "Coordinator - Creatives And Design", "9060459361", "https://www.facebook.com/nikhil.george.102", R.drawable.nikhil_george));
@@ -64,7 +64,7 @@ public class TeamFragment extends Fragment {
         allMembers.add(new info.anwesha.iitp.team.TeamItem("D K Gurjar", "Coordinator - Registrations And Security", "9352052504", "https://www.dontUseFacebook.com", R.drawable.dk));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Neeraj Paraliya", "Coordinator - Registrations And Security", "0", "https://www.facebook.com/profile.php?id=100013775678152", R.drawable.neeraj));
         allMembers.add(new info.anwesha.iitp.team.TeamItem("Anuj Shastri", "Coordinator - Registrations And Security", "0", "https://www.facebook.com/anuj.shastri.3910", R.drawable.anuj_shastri));
-
+        
 
         adapter.setTeamItemList(allMembers);
 
